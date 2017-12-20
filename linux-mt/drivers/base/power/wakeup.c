47e6be8c6f5584c07aa93b9665b31bebe346d3fa
frank <frankwu@gmx.de>
2017-12-20 16:34:28 +0100

    complete patches after upgrade

--- linux-mt/drivers/base/power/wakeup.c
+++ linux-mt/drivers/base/power/wakeup.c
@@ -1066,16 +1066,18 @@ static int print_wakeup_source_stats(struct seq_file *m,
 static int wakeup_sources_stats_show(struct seq_file *m, void *unused)
 {
 	struct wakeup_source *ws;
+	int srcuidx;
 
-	seq_puts(m, "name\t\t\t\t\tactive_count\tevent_count\twakeup_count\t"
+	seq_puts(m, "name\t\tactive_count\tevent_count\twakeup_count\t"
 		"expire_count\tactive_since\ttotal_time\tmax_time\t"
 		"last_change\tprevent_suspend_time\n");
 
-	rcu_read_lock();
+	//rcu_read_lock();
+	srcuidx = srcu_read_lock(&wakeup_srcu);
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry)
 		print_wakeup_source_stats(m, ws);
-	rcu_read_unlock();
-
+	//rcu_read_unlock();
+	srcu_read_unlock(&wakeup_srcu, srcuidx);
 	print_wakeup_source_stats(m, &deleted_ws);
 
 	return 0;
